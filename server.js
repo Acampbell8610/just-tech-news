224d937924970da955d17d314bba5a4b634679e0
acamp <acampbell8610@gmail.com>
2022-03-21 21:24:40 -0500

    partials/helpers

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const path = require('path');
 const session = require('express-session');
+const helpers = require('./utils/helper');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,7 +23,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
